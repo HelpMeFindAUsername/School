5474641e9eaace6b5d413b2f619343f2d973ca4c
HelpMeFindAUsername <126514483+HelpMeFindAUsername@users.noreply.github.com>
2025-01-14 14:53:38 +0100

    Add files via upload

--- Lucchini_PariDispari.cpp
+++ Lucchini_PariDispari.cpp
@@ -7,26 +7,30 @@
 
 #include <iostream>
 #include <cmath>
-#include <vector>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
     int vet[20];
-    int contaP = 0, contaD = 0;
     int vetP[20];
     int vetD[20];
+    int contaP = 0, contaD = 0;
     float media = 0;
+
+// Creazione dei 20 numeri casuali per il vettore
     srand(time(0));
     for(int i=0; i < 20; i++){
         vet[i] = rand() % 51;
     }
-    
+
+// Inizializzazione dei vettori vetP e vetD
     for(int i = 0; i < 20; i++){
         vetP[i] = 99;
         vetD[i] = 99;
     }
-    
+
+// Assegnazione dei valori pari e dispari ai vettori vetP e vetD
     for(int i = 0; i < 20; i++){
         
         if(vet[i]%2 == 0){
@@ -38,40 +42,52 @@ int main() {
             contaD++;
         }
     }
-    cout << "Numeri: ";
-    
+
+// Stampa dei vettori
+    cout << "Numeri:          ";
+
     for (int i = 0; i < 20; i++) {
-       cout << vet[i] << "  ";
+       cout << i << ": " << vet[i];
+       if(i < 19)
+        cout << ", ";
     }
-    cout << endl;
-    cout << "Numeri pari: ";
+    cout << "\n\n";
+    cout << "Numeri pari:     ";
     
-    for (int i = 0; i < 20 && vetP[i] != 99; i++) {
-       cout << vetP[i] << "  ";
+     for (int i = 0; i < contaP; i++) {
+       cout << i << ": " << vetP[i];
+       if(i < contaP-1)
+        cout << ", ";
     }
-    cout << endl;
-    cout << "Numeri dispari: ";
+    cout << "\n\n";
+    cout << "Numeri dispari:  ";
     
-    for (int i = 0; i < 20 && vetD[i] != 99; i++) {
-       cout << vetD[i] << "  ";
+     for (int i = 0; i < contaD; i++) {
+       cout << i << ": " << vetD[i];
+       if(i < contaD-1)
+        cout << ", ";
     }
-    cout << endl;
     
     for(int i = 0; i < contaP; i++){
         media += vetP[i];
     }
+
+
     if (media != 0) {
         media /= contaP;
-        cout << "Media pari: " << media << endl;
+        cout << "\n\nMedia pari: " << media << endl;
     }
     else
-        cout << "Non ci sono valori pari" << endl;
+        cout << "\n\nNon ci sono valori pari" << endl;
     
     media = 0;
     
+
     for(int i = 0; i < contaD; i++){
         media += vetD[i];
     }
+
+    
     if (media != 0) {
         media /= contaD;
         cout << "Media dispari: " << media << endl;
