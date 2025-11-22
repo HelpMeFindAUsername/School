65d8a3b7786fea61e1e659474ffdbdd9006f17be
Alberto Lucchini <albilucchi@icloud.com>
2025-11-22 08:12:44 +0100

    AAAAAAA

---
+++ CPP/Lucchini_autosalone.cpp
@@ -0,0 +1,210 @@
+//Alberto Lucchini
+//Progetto Autosalone
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+
+using namespace std;
+
+const int DIM = 5;
+int spazioOcc = 0;
+
+
+struct Auto{
+    string marca;
+    int cilindrata;
+    int annoImmatricolazione;
+    
+    struct acquirente{
+        string nome;
+        string cognome;
+        string codiceFiscale;
+    } acq;
+};
+
+void gotoxy(int x, int y);
+
+int menu();
+
+void initVettAuto(Auto autosalone[], int dim);
+
+void insAuto(Auto autosalone[], int &dim);
+void visCognomi(Auto autosalone[], int dim, int cilindrataMin);
+void visAutoPerAnno(Auto autosalone[], int dim, int anno);
+void visTabella(Auto autosalone[], int dim);
+
+int main(){
+    Auto autosalone[DIM];
+    int scelta;
+    int anno;
+    const int cilindrataMin = 1500;
+
+    initVettAuto(autosalone, DIM);
+
+    do{
+        scelta = menu();
+        switch(scelta){
+            case 1:
+                insAuto(autosalone, spazioOcc);
+                break;
+            case 2:
+                visCognomi(autosalone, spazioOcc, cilindrataMin);
+                break;
+            case 3:
+                cout << "Inserisci l'anno di immatricolazione da cercare: ";
+                cin >> anno;
+                visAutoPerAnno(autosalone, spazioOcc, anno);
+                break;
+            case 4:
+                visTabella(autosalone, spazioOcc);
+                break;
+            case 0:
+                cout << "Uscita in corso...\n";
+                break;
+            default:
+                cout << "Scelta non valida. Riprova.\n";
+        }
+    }while(scelta != 0);
+
+    return 0;
+}
+
+void gotoxy(int x, int y){
+     HANDLE HConsole;
+     CONSOLE_SCREEN_BUFFER_INFO ConsoleInfo;
+     HConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+     ConsoleInfo.dwCursorPosition.X = x;
+     ConsoleInfo.dwCursorPosition.Y = y;
+     SetConsoleCursorPosition(HConsole,ConsoleInfo.dwCursorPosition);
+}
+
+void initVettAuto(Auto autosalone[], int dim){
+    for(int i = 0; i < dim; i++){
+        autosalone[i].marca = "";
+        autosalone[i].cilindrata = 0;
+        autosalone[i].annoImmatricolazione = 0;
+        autosalone[i].acq.nome = "";
+        autosalone[i].acq.cognome = "";
+        autosalone[i].acq.codiceFiscale = "";
+    }
+}
+
+int menu(){
+    int scelta;
+    system("cls");
+    cout << "1. Inserisci auto\n";
+    cout << "2. Visualizza o cognome degli acquirenti di auto di cilindrata superiore a 1500 cc\n";
+    cout << "3. Visualizza auto immatricolate in un anno specifico\n";
+    cout << "4. Visualizza l'intera tabella\n";
+    cout << "0. Uscita\n";
+    cout << "Inserisci la tua scelta: ";
+    cin >> scelta;
+    return scelta;
+}
+
+void insAuto(Auto autosalone[], int &dim){
+    int numAuto;
+    cout << "Quante auto vuoi inserire? Max " << DIM - dim << ": ";
+    cin >> numAuto;
+
+    while(numAuto > DIM - dim || numAuto < 1){
+        cout << "Numero non valido. Inserisci un numero tra 1 e " << DIM - dim << ": ";
+        cin >> numAuto;
+    }
+
+    int start = dim;
+    int end = start + numAuto;
+    for(int i = start; i < end; i++){
+        cout << "Inserisci la marca dell'auto: ";
+        cin >> autosalone[i].marca;
+        cout << "Inserisci la cilindrata dell'auto: ";
+        cin >> autosalone[i].cilindrata;
+        cout << "Inserisci l'anno di immatricolazione dell'auto: ";
+        cin >> autosalone[i].annoImmatricolazione;
+        cout << "Inserisci il nome dell'acquirente: ";
+        cin >> autosalone[i].acq.nome;
+        cout << "Inserisci il cognome dell'acquirente: ";
+        cin >> autosalone[i].acq.cognome;
+        cout << "Inserisci il codice fiscale dell'acquirente: ";
+        cin >> autosalone[i].acq.codiceFiscale;
+        cout << "Auto " << (i - start + 1) << " inserita con successo!\n";
+        cout << "---------------------------\n";
+    }
+
+    dim = end; // aggiorna il contatore reale delle auto
+
+    cout << "Tutte le auto sono state inserite. Digita 0 per tornare al menu'... ";
+    int temp;
+    cin >> temp;
+}
+
+void visCognomi(Auto autosalone[], int dim, int cilindrataMin){
+    bool found = false;
+    system("cls");
+    cout << "Auto con cilindrata superiore a " << cilindrataMin << " cc:\n";
+    cout << left << setw(15) << "Marca" << setw(12) << "Cilindrata" << setw(8) << "Anno" << setw(15) << "Nome" << setw(15) << "Cognome" << setw(20) << "Cod.Fiscale" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+
+    for(int i = 0; i < dim; i++){
+        if(autosalone[i].cilindrata > cilindrataMin){
+            cout << left << setw(15) << autosalone[i].marca 
+                 << setw(12) << autosalone[i].cilindrata 
+                 << setw(8) << autosalone[i].annoImmatricolazione 
+                 << setw(15) << autosalone[i].acq.nome
+                 << setw(15) << autosalone[i].acq.cognome
+                 << setw(20) << autosalone[i].acq.codiceFiscale
+                 << endl;
+            found = true;
+        }
+    }
+    if(!found){
+        cout << "\nNessuna auto trovata con cilindrata superiore a " << cilindrataMin << " cc.\n";
+    }
+    cout << "Digita 0 per tornare al menu'...";
+    int temp;
+    cin >> temp;
+}
+
+void visAutoPerAnno(Auto autosalone[], int dim, int anno){
+    bool found = false;
+    system("cls");
+    cout << "Auto immatricolate nell'anno " << anno << ":\n";
+    cout << left << setw(15) << "Marca" << setw(12) << "Cilindrata" << setw(8) << "Anno" << setw(15) << "Nome" << setw(15) << "Cognome" << setw(20) << "Cod.Fiscale" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+    for(int i = 0; i < dim; i++){
+        if(autosalone[i].annoImmatricolazione == anno){
+            cout << left << setw(15) << autosalone[i].marca 
+                 << setw(12) << autosalone[i].cilindrata 
+                 << setw(8) << autosalone[i].annoImmatricolazione 
+                 << setw(15) << autosalone[i].acq.nome
+                 << setw(15) << autosalone[i].acq.cognome
+                 << setw(20) << autosalone[i].acq.codiceFiscale
+                 << endl;
+            found = true;
+        }
+    }
+    if(!found){
+        cout << "\nNessuna auto trovata per l'anno " << anno << ".\n";
+    }
+    cout << "Digita 0 per tornare al menu'...";
+    int temp;
+    cin >> temp;
+}
+
+void visTabella(Auto autosalone[], int dim){
+    system("cls");
+    cout << left << setw(15) << "Marca" << setw(12) << "Cilindrata" << setw(8) << "Anno" << setw(15) << "Nome" << setw(15) << "Cognome" << setw(20) << "Cod.Fiscale" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+    for(int i = 0; i < dim; i++){
+        cout << left << setw(15) << autosalone[i].marca 
+             << setw(12) << autosalone[i].cilindrata 
+             << setw(8) << autosalone[i].annoImmatricolazione 
+             << setw(15) << autosalone[i].acq.nome
+             << setw(15) << autosalone[i].acq.cognome
+             << setw(20) << autosalone[i].acq.codiceFiscale
+             << endl;
+    }
+    cout << "Digita 0 per tornare al menu'...";
+    int temp;
+    cin >> temp;
+}
--- CPP/Lucchini_biblioteca.cpp
+++ CPP/Lucchini_biblioteca.cpp
@@ -1,22 +1,36 @@
 // LUCCHINI ALBERTO
 
+// Posizione stampa
+// 0;    15; 20;     32;     60;   62;   66;   73;
+// ISBN; ID; TITOLO; AUTORE; MESE; ANNO; SEDE; CASA;
+
+
 #include <iostream>
 #include <iomanip>
+#include <windows.h>
 
 using namespace std;
 
-int personaCercata;
-string libroCercato;
-
 const int DIM = 3;
+const int ORD[] = {0, 15, 20, 32, 60, 65, 75, 82};
+
+bool uscita = 0;
+
+int ver = 0;
+
+int spazioOcc = 0;
+int inp;
+int indexTrovato;
+
+string ricerca;
 
 struct libri{
     struct codiceLibro{
-        string isbn[20];
-        int id;
+        string isbn;
+        string id;
     }cLibro;
 
-    string titolo[60];
+    string titolo;
 
     string autore;
 
@@ -26,19 +40,234 @@ struct libri{
     } aPub;
 
     struct  Editore{
-        string sede[40];
-        string cEd[40];
-    } genitore1;
+        string sede;
+        string cEd;
+    } editore;
 };
+
+libri biblioteca[DIM];
+
+void gotoxy(int x, int y);
+void insLibro(libri biblioteca[], int dim);
+int cercaLibro(libri biblioteca[], int dim, int input, string libroCercato);
+int stampa(libri biblioteca[], int dim, int& ver, bool cS, int iT);
+
 
 int menu();
 
 int main(){
+    while (!uscita)
+    {
+        string ricerca;
+        switch (menu()){
+            case 1:
+                if(DIM - spazioOcc > 0){
+                    do{
+                        cout << "Quanti libri vuoi aggiungere? (Spazio disponibile: " << DIM - spazioOcc << ", 0 per tornare al menu'): ";
+                        cin >> inp;
+                        if ((inp < 1 || inp > DIM - spazioOcc) && inp != 0)
+                        {
+                            cout << "Non e' disponibile abbastanza spazio!";
+                        }
+                        else if (inp == 0){
+                            break;
+                        }
+                        
+                        
+                    }while (inp < 1 || inp > DIM - spazioOcc);
+                
+                    insLibro(biblioteca, inp);
+                }
+                else{
+                    cout << "Non e' disponibile abbastanza spazio!";
+                }
+                break;
 
+            case 2:
+                cout << "Inserisci l'isbn o ID del libro da cercare:";
+                cin >> ricerca;
+                indexTrovato = cercaLibro(biblioteca, spazioOcc, 1, ricerca);
+                if(indexTrovato == -1){
+                    cout << "Nessun libro trovato con quel isbn o ID\nPremere invio per tornare al menu':";
+                    cin.get();
+                    cin.get();
+
+                }
+                else{
+                    stampa(biblioteca, spazioOcc, ver, 0, indexTrovato);
+                }
+                break;
+
+            case 3:
+                cout << "Inserisci il cognome da cercare:";
+                cin >> ricerca;
+                indexTrovato = cercaLibro(biblioteca, spazioOcc, 2, ricerca);
+                if(indexTrovato == -1){
+                    cout << "Nessun libro trovato con quel congnome\nPremere invio per tornare al menu':";
+                    cin.get(); 
+                    cin.get();
+                }
+                else{
+                    stampa(biblioteca, spazioOcc, ver, 0, indexTrovato);
+                }
+                break;
+
+            case 4:
+                stampa(biblioteca, spazioOcc, ver, 1, 0);
+                break;
+
+            case 0:
+                uscita = 1;
+                break;
+            
+            default:
+                
+                break;
+        }
+    }
+    
+    return 0;
+}
+
+void gotoxy(int x, int y){
+     HANDLE HConsole;
+     CONSOLE_SCREEN_BUFFER_INFO ConsoleInfo;
+     HConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+     ConsoleInfo.dwCursorPosition.X = x;
+     ConsoleInfo.dwCursorPosition.Y = y;
+     SetConsoleCursorPosition(HConsole,ConsoleInfo.dwCursorPosition);
+}
+
+void insLibro(libri biblioteca[], int dim){
+    for(int i = 0; i < dim; i++){
+        cout << "Inserisci il codice ISBN del libro: ";
+        cin >> biblioteca[i].cLibro.isbn;
+        cout << "Inserisci l'ID del libro: ";
+        cin >> biblioteca[i].cLibro.id;
+        cout << "Inserisci il titolo del libro: ";
+        cin >> biblioteca[i].titolo;
+        cout << "Inserisci l'autore del libro: ";
+        cin >> biblioteca[i].autore;
+        cout << "Inserisci il mese di pubblicazione del libro: ";
+        cin >> biblioteca[i].aPub.mese;
+        cout << "Inserisci l'anno di pubblicazione del libro: ";
+        cin >> biblioteca[i].aPub.anno;
+        cout << "Inserisci la sede dell'editore del libro: ";
+        cin >> biblioteca[i].editore.sede;
+        cout << "Inserisci la casa editrice del libro: ";
+        cin >> biblioteca[i].editore.cEd;
+        spazioOcc++;
+    }
+}
+
+int cercaLibro(libri biblioteca[], int dim, int input, string libroCercato){
+    if(input == 1){
+        for (int i = 0; i < dim; i++)
+        {
+            if(libroCercato == biblioteca[i].cLibro.id || libroCercato == biblioteca[i].cLibro.isbn){
+                return i;
+            }
+        }
+    }
+    else if(input == 2){
+        for (int i = 0; i < dim; i++)
+        {
+            if(libroCercato == biblioteca[i].autore){
+                return i;
+            }
+        }
+    }
+
+    return -1;
+
+}
+
+int stampa(libri biblioteca[], int dim, int& ver, bool cS, int iT){
+    system("cls");
+    int scelta;
+    ver = 0;
+    
+    gotoxy(ORD[0], ver);
+    cout << "Codice libro";
+    gotoxy(ORD[4], ver);
+    cout << "Anno";
+    ver++;
+    gotoxy(ORD[4], ver);
+    cout << "Pubblicazione";
+    ver--;
+    gotoxy(ORD[6], ver);
+    cout << "Editore";
+    ver++;
+    gotoxy(ORD[2], ver);
+    cout << "Titolo";
+    gotoxy(ORD[3], ver);
+    cout << "Autore";
+    ver++;
+    gotoxy(ORD[0], ver);
+    cout << "isbn";
+    gotoxy(ORD[1], ver);
+    cout << "ID";
+    gotoxy(ORD[4], ver);
+    cout << "Mese";
+    gotoxy(ORD[5], ver);
+    cout << "Anno";
+    gotoxy(ORD[6], ver);
+    cout << "Sede";
+    gotoxy(ORD[7], ver);
+    cout << "Casa Editrice";
+    ver++;
+    if (cS)
+    {
+        for(int i = 0; i < dim; i++){
+            gotoxy(ORD[0], ver);
+            cout << biblioteca[i].cLibro.isbn;
+            gotoxy(ORD[1], ver);
+            cout << biblioteca[i].cLibro.id;
+            gotoxy(ORD[2], ver);
+            cout << biblioteca[i].titolo;
+            gotoxy(ORD[3], ver);
+            cout << biblioteca[i].autore;
+            gotoxy(ORD[4], ver);
+            cout << biblioteca[i].aPub.mese;
+            gotoxy(ORD[5], ver);
+            cout << biblioteca[i].aPub.anno;
+            gotoxy(ORD[6], ver);
+            cout << biblioteca[i].editore.sede;
+            gotoxy(ORD[7], ver);
+            cout << biblioteca[i].editore.cEd;
+            ver++;
+        }
+    }
+    else{
+        gotoxy(ORD[0], ver);
+        cout << biblioteca[iT].cLibro.isbn;
+        gotoxy(ORD[1], ver);
+        cout << biblioteca[iT].cLibro.id;
+        gotoxy(ORD[2], ver);
+        cout << biblioteca[iT].titolo;
+        gotoxy(ORD[3], ver);
+        cout << biblioteca[iT].autore;
+        gotoxy(ORD[4], ver);
+        cout << biblioteca[iT].aPub.mese;
+        gotoxy(ORD[5], ver);
+        cout << biblioteca[iT].aPub.anno;
+        gotoxy(ORD[6], ver);
+        cout << biblioteca[iT].editore.sede;
+        gotoxy(ORD[7], ver);
+        cout << biblioteca[iT].editore.cEd;
+        ver++;
+    }
+
+    ver += 2;
+    gotoxy(ORD[0], ver);
+    cout << "Per tornare al menu' premere invio: ";
+    cin.get();
+    cin.get();
     return 0;
 }
 
 int menu(){
+    system("cls");
     int scelta;
 
     cout << "MENU'" << endl;
